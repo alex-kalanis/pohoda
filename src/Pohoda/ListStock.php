a922529147c1f4df9d55f0c8c2e83cfd5551eec2
Petr Plsek <me@kalanys.com>
2025-08-11 07:25:52 +0200

    Add list stock initial

---
+++ src/Pohoda/ListStock.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * This file is part of riesenia/pohoda package.
+ *
+ * Licensed under the MIT License
+ * (c) RIESENIA.com
+ */
+
+declare(strict_types=1);
+
+namespace Riesenia\Pohoda;
+
+use Riesenia\Pohoda\Common\OptionsResolver;
+use Symfony\Component\OptionsResolver\Options;
+
+class ListStock extends AbstractAgenda
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getXML(): \SimpleXMLElement
+    {
+        $xml = $this->createXML()->addChild($this->data['namespace'] . ':list' . $this->data['type'], '', $this->namespace(strval($this->data['namespace'])));
+        $xml->addAttribute('version', '2.0');
+
+        if ($this->data['timestamp']) {
+            $date = $this->data['timestamp'];
+            if (is_a($date, \DateTimeInterface::class)) {
+                $date = $date->format('Y-m-d\TH:i:s');
+            }
+            $xml->addAttribute('dateTimeStamp', $date);
+        }
+
+        if ($this->data['validFrom']) {
+            $dateFrom = $this->data['validFrom'];
+            if (is_a($dateFrom, \DateTimeInterface::class)) {
+                $dateFrom = $dateFrom->format('Y-m-d');
+            }
+            $xml->addAttribute('dateValidFrom', $dateFrom);
+        }
+
+        if ($this->data['state']) {
+            $xml->addAttribute('state', $this->data['state']);
+        }
+
+        return $xml;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureOptions(OptionsResolver $resolver): void
+    {
+        // available options
+        $resolver->setDefined(['type', 'namespace', 'timestamp', 'validFrom', 'state']);
+
+        // validate / format options
+        $resolver->setRequired('type');
+        $resolver->setNormalizer('type', $this->normalizerFactory->getClosure('list_request_type'));
+        $resolver->setDefault('namespace', function (Options $options) {
+            return 'lst';
+        });
+    }
+}
---
+++ tests/AgendaTests/ListStockTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace tests\AgendaTests;
+
+use tests\CommonTestClass;
+use Riesenia\Pohoda;
+use Riesenia\Pohoda\ValueTransformer;
+
+class ListStockTest extends CommonTestClass
+{
+
+    public function testSimple(): void
+    {
+        $lib = new Pohoda\ListStock(new Pohoda\Common\NamespacesPaths(), new ValueTransformer\SanitizeEncoding(new ValueTransformer\Listing()), [
+            'type' => 'Stock',
+            'timestamp' => date_create_immutable('2025-07-01T01:00:00'),
+            'validFrom' => date_create_immutable('2025-07-01'),
+            'state' => 'ok',
+        ], '123');
+        $this->assertEquals('<lst:listStock version="2.0" dateTimeStamp="2025-07-01T01:00:00" dateValidFrom="2025-07-01" state="ok"/>', $lib->getXML()->asXML());
+//        $this->assertEquals(, $lib->getXML()->asXML());
+    }
+}
