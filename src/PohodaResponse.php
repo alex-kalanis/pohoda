2c090c8510aab429a179e0bc00ac10773761d7ee
Petr Plsek <me@kalanys.com>
2025-08-11 07:36:17 +0200

    Add packing as response
    Extended request with this

---
+++ src/PohodaResponse.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This file is part of riesenia/pohoda package.
+ *
+ * Licensed under the MIT License
+ * (c) RIESENIA.com
+ */
+
+declare(strict_types=1);
+
+namespace Riesenia;
+
+
+use Riesenia\Pohoda\AbstractAgenda;
+
+
+/**
+ * Factory for Pohoda objects.
+ */
+class PohodaResponse extends Pohoda
+{
+
+    /**
+     * Open new XML file for writing.
+     *
+     * @param string|null $filename path to output file or null for memory
+     * @param string      $id
+     * @param string      $note
+     * @param string      $state
+     * @param string|null $programVersion
+     * @param string|null $key
+     *
+     * @return bool
+     */
+    public function open(?string $filename, string $id, string $note = '', string $state = 'ok', ?string $programVersion = null, ?string $key = null): bool
+    {
+        $this->xmlWriter = new \XMLWriter();
+
+        if (is_null($filename)) {
+            $this->isInMemory = true;
+            $this->xmlWriter->openMemory();
+        } else {
+            $this->isInMemory = false;
+
+            if (!$this->xmlWriter->openUri($filename)) {
+                // @codeCoverageIgnoreStart
+                // I cannot test this, because it needs source file somewhere online
+                return false;
+            }
+            // @codeCoverageIgnoreEnd
+        }
+
+        $this->xmlWriter->startDocument('1.0', $this->sanitizeEncoding->getEncoding());
+        $this->xmlWriter->startElementNs('rsp', 'responsePack', null);
+
+        $this->xmlWriter->writeAttribute('id', $id);
+        $this->xmlWriter->writeAttribute('ico', $this->companyRegistrationNumber);
+        $this->xmlWriter->writeAttribute('application', $this->application);
+        $this->xmlWriter->writeAttribute('version', '2.0');
+        $this->xmlWriter->writeAttribute('note', $note);
+        $this->xmlWriter->writeAttribute('state', $state);
+        if (!is_null($programVersion)) {
+            $this->xmlWriter->writeAttribute('programVersion', $programVersion);
+        }
+        if (!is_null($key)) {
+            $this->xmlWriter->writeAttribute('key', $key);
+        }
+
+        foreach ($this->namespacesPaths->allNamespaces() as $k => $v) {
+            // put all known namespaces into base element attributes
+            $this->xmlWriter->writeAttributeNs('xmlns', $k, null, $v);
+        }
+
+        return true;
+    }
+
+    /**
+     * Add item.
+     *
+     * @param string $id
+     * @param AbstractAgenda $agenda
+     * @param string $state
+     *
+     * @return void
+     */
+    public function addItem(string $id, AbstractAgenda $agenda, string $state = 'ok'): void
+    {
+        $this->xmlWriter->startElementNs('rsp', 'responsePackItem', null);
+
+        $this->xmlWriter->writeAttribute('id', $id);
+        $this->xmlWriter->writeAttribute('version', '2.0');
+        $this->xmlWriter->writeAttribute('state', $state);
+        $this->xmlWriter->writeRaw((string) $agenda->getXML()->asXML());
+        $this->xmlWriter->endElement();
+
+        if (!$this->isInMemory) {
+            $this->xmlWriter->flush();
+        }
+    }
+}
---
+++ tests/BasicTests/PohodaResponseTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace tests\BasicTests;
+
+use tests\CommonTestClass;
+use Riesenia\Pohoda\Common\NamespacesPaths;
+use Riesenia\Pohoda\Stock;
+use Riesenia\Pohoda\ValueTransformer;
+use Riesenia\PohodaResponse;
+
+class PohodaResponseTest extends CommonTestClass
+{
+    protected ValueTransformer\SanitizeEncoding $sanitization;
+    protected ?string $tempFile = null;
+
+    protected function setUp(): void
+    {
+        $this->sanitization = new ValueTransformer\SanitizeEncoding(new ValueTransformer\Listing());
+    }
+
+    protected function tearDown(): void
+    {
+        if ($this->tempFile) {
+            @unlink($this->tempFile);
+        }
+    }
+
+    public function testWriteFile(): void
+    {
+        $this->tempFile = \tempnam(\sys_get_temp_dir(), 'xml');
+
+        $stock = new Stock(new NamespacesPaths(), $this->sanitization, [
+            'code' => 'CODE',
+            'name' => 'NAME',
+            'storage' => 'STORAGE',
+            'typePrice' => ['id' => 1]
+        ], '123');
+
+        $lib = $this->getLib();
+        $this->assertTrue($lib->open($this->tempFile, 'ABC'));
+        $lib->addItem('ITEM_ID', $stock, 'test');
+        $lib->close();
+
+        $xml = \simplexml_load_file($this->tempFile);
+
+        // test dataPack properties
+        $this->assertEquals('ABC', strval($xml['id']));
+        $this->assertEquals('123', strval($xml['ico']));
+        $this->assertEquals('', strval($xml['note']));
+
+        // test dataPackItem properties
+        $this->assertEquals('ITEM_ID', strval($xml->children('rsp', true)->responsePackItem->attributes()['id']));
+    }
+
+    public function testCanWriteToMemory(): void
+    {
+        $stock = new Stock(new NamespacesPaths(), $this->sanitization, [
+            'code' => 'CODE',
+            'name' => 'NAME',
+            'storage' => 'STORAGE',
+            'typePrice' => ['id' => 1]
+        ], '123');
+
+        $lib = $this->getLib();
+        $this->assertTrue($lib->open(null, 'ABC'));
+        $lib->addItem('ITEM_ID', $stock, 'test');
+
+        $xml = \simplexml_load_string($lib->close());
+
+        // test dataPack properties
+        $this->assertEquals('ABC', strval($xml['id']));
+        $this->assertEquals('123', strval($xml['ico']));
+        $this->assertEquals('', strval($xml['note']));
+
+        // test dataPackItem properties
+        $this->assertEquals('ITEM_ID', strval($xml->children('rsp', true)->responsePackItem->attributes()['id']));
+    }
+
+    public function testRunTransformersProperly(): void
+    {
+        $stock = new Stock(new NamespacesPaths(), $this->sanitization, [
+            'code' => 'code1',
+            'name' => 'name2',
+            'storage' => 'storage3',
+            'typePrice' => ['id' => 4]
+        ], '123');
+
+        $lib = $this->getLib();
+        // set for each run
+        $lib->getTransformerListing()->clear();
+        $lib->getTransformerListing()->addTransformer(new XCapitalize());
+
+        $this->assertTrue($lib->open(null, 'ABC'));
+        $lib->addItem('item_id', $stock, 'test');
+
+        $xml = \simplexml_load_string($lib->close());
+
+        $this->assertEquals('CODE1', strval($xml->xpath('//stk:code')[0]));
+        $this->assertEquals('NAME2', strval($xml->xpath('//stk:name')[0]));
+        $this->assertEquals('STORAGE3', strval($xml->xpath('//typ:ids')[0]));
+
+        // Don't add transformers to other tests
+        $lib->getTransformerListing()->clear();
+    }
+
+    public function testHandleSanitizeCorrectly(): void
+    {
+        $stock = new Stock(new NamespacesPaths(), $this->sanitization, [
+            'code' => 'code1',
+            'name' => 'name2',
+            'storage' => 'storage3',
+            'typePrice' => ['id' => 4]
+        ], '123');
+
+        $this->sanitization->willBeSanitized(true);
+
+        $lib = $this->getLib();
+        $this->assertTrue($lib->open(null, 'ABC', '', 'ok', 'test-123', '999-888-777-666-555-444-333-222-111'));
+        $lib->addItem('item_id', $stock, 'test');
+        $this->assertEquals(0, \count($this->sanitization->getListing()->clear()->getTransformers()));
+        $lib->close();
+    }
+
+    public function getLib(): PohodaResponse
+    {
+        return new PohodaResponse(
+            '123', $this->sanitization,
+        );
+    }
+}
